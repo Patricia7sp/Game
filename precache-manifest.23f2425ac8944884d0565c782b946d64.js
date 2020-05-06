fc73c2fde0a3bb8f46b8744328e5e1705415dcc5
Patricia de Menezes <paty7sp@gmail.com>
2020-05-06 10:51:46 -0300

    Updates

---
+++ precache-manifest.23f2425ac8944884d0565c782b946d64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ccf0fcb6f1807c41dffc8028ca2c29",
+    "url": "/Game/index.html"
+  },
+  {
+    "revision": "d625151e81dfaebb52bd",
+    "url": "/Game/static/css/main.63f5be98.chunk.css"
+  },
+  {
+    "revision": "848c1662cdf72e95b517",
+    "url": "/Game/static/js/2.6e8ff824.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Game/static/js/2.6e8ff824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d625151e81dfaebb52bd",
+    "url": "/Game/static/js/main.8f74082b.chunk.js"
+  },
+  {
+    "revision": "0c019bf7620a9989e8eb",
+    "url": "/Game/static/js/runtime-main.55fed2f5.js"
+  },
+  {
+    "revision": "0b50d3d3fe594c4a4dc567ea14d87e23",
+    "url": "/Game/static/media/logo.0b50d3d3.svg"
+  }
+]);
